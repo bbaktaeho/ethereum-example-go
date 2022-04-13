4b394c072ad0ede0f28aacd9060c4b5ab9f367e7
bbaktaeho <lth9036@gmail.com>
2022-04-13 22:33:34 +0900

    update test code

--- util/ethers/ethers.util_test.go
+++ util/ethers/ethers.util_test.go
@@ -15,11 +15,11 @@ func TestWeiToEther(t *testing.T) {
 		args      args
 		wantEther *big.Float
 	}{
-		// TODO: Add test cases.
+		{"1", args{big.NewInt(1000000000000)}, big.NewFloat(0.000001)},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if gotEther := WeiToEther(tt.args.wei); !reflect.DeepEqual(gotEther, tt.wantEther) {
+			if gotEther := WeiToEther(tt.args.wei); !reflect.DeepEqual(gotEther.String(), tt.wantEther.String()) {
 				t.Errorf("WeiToEther() = %v, want %v", gotEther, tt.wantEther)
 			}
 		})
